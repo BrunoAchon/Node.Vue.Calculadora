e6ba522f2711097eb02e8a71baddd316ae18120d
unknown <brunoachon@hotmail.com>
2022-03-24 08:46:38 -0300

    inclusão de styles dos botoes

---
+++ calculator/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <h1>Calculator</h1>
+    <Calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from "./main/Calculator.vue";
+
+export default {
+  components: { Calculator },
+};
+</script>
+
+<style>
+@font-face {
+  font-family: "RobotoMono";
+  src: url("./fonts/RobotoMono-Thin.ttf");
+}
+
+* {
+  font-family: "RobotoMono", monospace;
+}
+
+body {
+  
+  margin: 0;
+}
+
+#app {
+  align-content: center;
+  align-items: center;
+
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  text-align: center;
+
+  background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 46, 73));
+}
+</style>
---
+++ calculator/src/main/Calculator.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="calculator">
+        <Display />
+        <Button label="AC" triple/>
+        <Button label="/" operation/>
+        <Button label="7"/>
+        <Button label="8"/>
+        <Button label="9"/>
+        <Button label="*" operation/>
+        <Button label="4"/>
+        <Button label="5"/>
+        <Button label="6"/>
+        <Button label="-" operation/>
+        <Button label="1"/>
+        <Button label="2"/>
+        <Button label="3"/>
+        <Button label="+" operation/>
+        <Button label="0" double/>
+        <Button label="."/>
+        <Button label="=" operation/>
+    </div>
+</template>
+
+<script>
+import Display from "../components/Display.vue";
+import Button from "../components/Button.vue";
+
+export default {
+    name: "calculator-item",
+    components: { Button, Display }
+}
+</script>
+
+<style>
+.calculator{
+    border-radius: 5px;
+    display: grid;
+    grid-template-columns: repeat(4,25%);
+    grid-template-rows: 1fr 48px 48px 48px 48px 48px ;
+    height: 320px;
+    overflow: hidden;
+    width: 235px;
+}
+</style>
